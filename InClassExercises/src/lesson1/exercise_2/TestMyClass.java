57960c0a89fda29630f9a72e4fe5830380b4216a
Suman Adhikari <expertxkid@gmail.com>
2024-02-26 12:47:49 -0600

    feat: complete lesson1 - exercise_2 on 20240226122100

--- InClassExercises/src/lesson1/exercise_2/TestMyClass.java
+++ InClassExercises/src/lesson1/exercise_2/TestMyClass.java
@@ -2,14 +2,47 @@ package lesson1.exercise_2;
 
 import org.junit.Test;
 
+import static org.junit.Assert.*;
+
+import lesson1.exercise_1.MyClass;
+
 public class TestMyClass {
 	@Test
 	public void testProduct() {
+		// Arrange
+		var numbers = new int[]{2,4,6};
+		int expected = 48;
+		
+		// Act
+		int result = MyClass.product(numbers);
+		
+		// Assert
+		assertEquals(expected, result);
+	}
 	
+	@Test
+	public void testProductZero() {
+		// Arrange
+		var numbers = new int[]{};
+		int expected = 0;
+		
+		// Act
+		int result = MyClass.product(numbers);
+		
+		// Assert
+		assertEquals(expected, result);
 	}
 	
 	@Test
 	public void testConcatenate() {
-	
+		// Arrange
+		String[] strings = {"Hello",", ", "World!"};
+		String expected = "Hello, World!";
+		
+		// Act
+		String result = MyClass.concatenate(strings);
+		
+		// Assert
+		assertTrue(expected.equals(result));
 	}
 }
