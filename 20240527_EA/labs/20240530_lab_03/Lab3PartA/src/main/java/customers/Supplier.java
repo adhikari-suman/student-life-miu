6912ef10d84ec448c30770fb187dfcda1350ceee
Suman Adhikari <expertxkid@gmail.com>
2024-05-30 15:56:04 -0500

    feat/EA: complete Lab3PartA

---
+++ 20240527_EA/labs/20240530_lab_03/Lab3PartA/src/main/java/customers/Supplier.java
@@ -0,0 +1,48 @@
+package customers;
+
+import java.util.List;
+
+public class Supplier {
+    private int id;
+    private String name;
+    private String phone;
+
+    public Supplier(int id, String name, String phone) {
+        this.id = id;
+        this.name = name;
+        this.phone = phone;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    @Override
+    public String toString() {
+        return "Supplier{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", phone='" + phone + '\'' +
+                '}';
+    }
+}
