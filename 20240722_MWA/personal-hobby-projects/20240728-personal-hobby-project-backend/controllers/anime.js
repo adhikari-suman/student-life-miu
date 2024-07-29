379575d5809b8e40ceb5c250275071dc78b6f74d
Suman Adhikari <expertxkid@gmail.com>
2024-07-28 20:06:55 -0500

    feat:PHP-MWA: complete FindOnePaginated for AnimeController

--- 20240722_MWA/personal-hobby-projects/20240728-personal-hobby-project-backend/controllers/anime.js
+++ 20240722_MWA/personal-hobby-projects/20240728-personal-hobby-project-backend/controllers/anime.js
@@ -1,32 +1,80 @@
-const findOne = function(req, res){
-    res.status(200).send("Found one anime");
+const { error } = require("console");
+const mongoose = require("mongoose");
+const MongoDBConstants = require("../constants").MongoDBConstants;
+const callbackify = require("util").callbackify;
+const animeModel = mongoose.model(MongoDBConstants.MODEL_ANIME);
+
+const anime_findAllPaginated = function (page, size) {
+  let offset = (page - 1) * size;
+
+  return animeModel.find().skip(offset).limit(size).exec();
+};
+
+const anime_findAllPaginatedCallback = callbackify(anime_findAllPaginated);
+
+const anime_onMongooseResponseCallback = function (req, res){
+    return function (error, anime) {
+        if (error) {
+          console.log(error);
+    
+          res.status(500).json({
+            error: "Something went wrong.",
+          });
+        } else {
+          res.status(200).json(anime);
+        }
+      };
 }
 
-const findAllWithPagination = function(req, res){
-    res.status(200).send("Found paginated animes");
-}
 
-const addOne = function(req, res){
-    res.status(200).send("Add one anime");
-}
+const findOne = function (req, res) {
+  res.status(200).send("Found one anime");
+};
 
-const updateOne = function(req, res){
-    res.status(200).send("Update one anime");
-}
+const findAllWithPagination = function (req, res) {
+  let page = parseInt(process.env.QUERY_PAGE_DEFAULT);
+  let size = parseInt(process.env.QUERY_SIZE_DEFAULT);
 
-const partiallyUpdateOne = function(req, res){
-    res.status(200).send("Patch one anime");
-}
+  if (req.query && req.query.page) {
+    page = parseInt(req.query.page);
+  }
 
-const deleteOne = function(req, res){
-    res.status(200).send("Deleted one anime");
-}
+  if (req.query && req.query.size) {
+    size = parseInt(req.query.size);
+  }
+
+  if (isNaN(page) || isNaN(size) || page < 1 || size < 1 || size > 20) {
+    res.status(400).json({
+      error:
+        "Page and size must be greater than one and size cannot be greater than 20.",
+    });
+    return;
+  }
+
+  anime_findAllPaginatedCallback(page, size, anime_onMongooseResponseCallback(req, res));
+};
+
+const addOne = function (req, res) {
+  res.status(200).send("Add one anime");
+};
+
+const updateOne = function (req, res) {
+  res.status(200).send("Update one anime");
+};
+
+const partiallyUpdateOne = function (req, res) {
+  res.status(200).send("Patch one anime");
+};
+
+const deleteOne = function (req, res) {
+  res.status(200).send("Deleted one anime");
+};
 
 module.exports = {
-    findOne,
-    findAllWithPagination,
-    addOne,
-    updateOne,
-    partiallyUpdateOne,
-    deleteOne
-}
+  findOne,
+  findAllWithPagination,
+  addOne,
+  updateOne,
+  partiallyUpdateOne,
+  deleteOne,
+};
