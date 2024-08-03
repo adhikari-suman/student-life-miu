dd44b3ba935a1518eeb21c9fca2f11eaa308879a
Suman Adhikari <expertxkid@gmail.com>
2024-08-03 10:34:03 -0500

    feat:PHP-MWA: update router

---
+++ 20240722_MWA/personal-hobby-projects/20240728-personal-hobby-project-backend/routes/anime.js
@@ -0,0 +1,29 @@
+const animeController = require("../controllers/anime");
+const characterController = require("../controllers/character");
+const router = require('express').Router();
+
+router
+    .route("")
+    .get(animeController.findAllWithPagination)
+    .post(animeController.addOne);
+
+router
+    .route("/:id")
+    .get(animeController.findOne)
+    .put(animeController.fullUpdateOne)
+    .patch(animeController.partiallyUpdateOne)
+    .delete(animeController.deleteOne);
+
+router
+    .route("/:id/characters")
+    .get(characterController.findAllWithPagination)
+    .post(characterController.addOne);
+
+router
+    .route("/:id/characters/:characterId")
+    .get(characterController.findOne)
+    .put(characterController.updateOne)
+    .patch(characterController.partiallyUpdateOne)
+    .delete(characterController.deleteOne);
+
+module.exports = router;
--- 20240722_MWA/personal-hobby-projects/20240728-personal-hobby-project-backend/routes/index.js
+++ 20240722_MWA/personal-hobby-projects/20240728-personal-hobby-project-backend/routes/index.js
@@ -1,30 +1,8 @@
 const router = require("express").Router();
 const animeController = require("../controllers/anime");
-const characterController = require("../controllers/character");
+const animeRouter = require("./anime");
 
 // dummy route for testing
-router
-  .route("/animes")
-  .get(animeController.findAllWithPagination)
-  .post(animeController.addOne);
-
-router
-  .route("/animes/:id")
-  .get(animeController.findOne)
-  .put(animeController.updateOne)
-  .patch(animeController.partiallyUpdateOne)
-  .delete(animeController.deleteOne);
-
-router
-  .route("/animes/:id/characters")
-  .get(characterController.findAllWithPagination)
-  .post(characterController.addOne);
-
-router
-  .route("/animes/:id/characters/:characterId")
-  .get(characterController.findOne)
-  .put(characterController.updateOne)
-  .patch(characterController.partiallyUpdateOne)
-  .delete(characterController.deleteOne);
+router.use('/anime', animeRouter);
 
 module.exports = router;
