3ec5064a174ce583b31783ac7b7abc6126634c8a
Suman Adhikari <expertxkid@gmail.com>
2024-08-08 16:52:57 -0500

    refactor/PHP-MWA: updated endpoints for character sub-set routes

--- 20240722_MWA/personal-hobby-projects/20240728-personal-hobby-project-backend/controllers/character.js
+++ 20240722_MWA/personal-hobby-projects/20240728-personal-hobby-project-backend/controllers/character.js
@@ -1,242 +1,321 @@
 const mongoose = require("mongoose");
 const ObjectId = mongoose.Types.ObjectId;
-const MongoDBConstants = require("../constants").MongoDBConstants;
 const callbackify = require("util").callbackify;
-const animeModel = mongoose.model(MongoDBConstants.MODEL_ANIME);
+const sendResponse = require('../utils/send_response');
+const Anime = mongoose.model(process.env.MONGODB_ANIME_MODEL_NAME);
+const characterDocumentName =
+    process.env.MONGODB_ANIME_CHARACTERS_DOCUMENT_NAME;
 
-const character_findAllPaginated = function (animeId, page, size) {
-  let offset = (page - 1) * size;
-  return animeModel
-    .findById(animeId)
-    .slice('characters', [offset, size])
-    .exec();
+const Anime_findById = function (animeId) {
+    return Anime.findById(animeId).exec();
+}
+
+const character_onMongooseFindOneResponseCallback = function (
+    req,
+    res,
+    characterId
+) {
+    return function (error, anime) {
+        if (error) {
+            console.log(error);
+            res.status(500).json({error: "Something went wrong."});
+        } else {
+            let character = anime.characters.id(characterId);
+
+            if (character == null) {
+                res.status(404).json({error: "Character not found."});
+            } else {
+                res.status(200).json(character);
+            }
+        }
+    };
 };
 
-const character_findById = function (animeId, characterId) {
-  return animeModel
-  .findById(animeId).select('characters').exec();
-};
-
-const character_addOne = function (animeId, character) {
-  return animeModel
-    .findByIdAndUpdate(animeId, { $push: { characters: character } }, { new: true, runValidators: true })
-    .exec();
-};
-
-const character_updateOne = function (animeId, characterId, character) {
-  return animeModel
-    .findOneAndUpdate({ _id: animeId, "characters._id": characterId }, { $set: { "characters.$": character } }, { new: true, runValidators: true })
-    .exec();
-};
-
-const character_deleteOne = function (animeId, characterId) {
-  return animeModel
-    .findByIdAndUpdate(animeId, { $pull: { characters: { _id: characterId } } }, { new: true })
-    .exec();
-};
-
-const character_findAllPaginatedCallback = callbackify(character_findAllPaginated);
-const character_findByIdCallback = callbackify(character_findById);
-const character_createCallback = callbackify(character_addOne);
-const character_updateOneCallback = callbackify(character_updateOne);
-const character_partiallyUpdateOneCallback = callbackify(character_updateOne);
-const character_deleteOneCallback = callbackify(character_deleteOne);
-
-const character_onMongooseResponseCallback = function (req, res) {
-  return function (error, character) {
-    if (error) {
-      console.log(error);
-      res.status(500).json({ error: "Something went wrong." });
-    } else {
-      if (character == null) {
-        res.status(404).json({ error: "Character not found." });
-      } else {
-        res.status(200).json(character);
-      }
+const findAll = function (req, res) {
+    const response = {
+        status: process.env.HTTP_STATUS_OKs,
+        data: null,
     }
-  };
+
+    const animeId = req.params.id;
+
+    Anime_findById(animeId).then(anime => {
+        if (anime === null) {
+            response.status = process.env.HTTP_STATUS_NOT_FOUND;
+            response.data = {
+                error: process.env.ERROR_RESPONSE_ANIME_NOT_FOUND,
+            }
+        } else {
+            response.status = process.env.HTTP_STATUS_OK;
+            response.data = anime.characters;
+        }
+    }).catch((error) => {
+        response.status = process.env.HTTP_STATUS_INTERNAL_SERVER_ERROR;
+        response.data = process.env.ERROR_RESPONSE_SOMETHING_WENT_WRONG;
+    }).finally(() => {
+        sendResponse(res, response);
+    });
+
 };
-
-const character_onMongooseFindAllResponseCallback = function (req, res) {
-    return function (error, anime) {
-      if (error) {
-        console.log(error);
-        res.status(500).json({ error: "Something went wrong." });
-      } else {
-        if (anime.characters == null) {
-          res.status(404).json({ error: "Character not found." });
-        } else {
-          res.status(200).json(anime.characters);
-        }
-      }
-    };
-  };
-
-  const character_onMongooseFindOneResponseCallback = function (req, res, characterId) {
-    return function (error, anime) {
-      if (error) {
-        console.log(error);
-        res.status(500).json({ error: "Something went wrong." });
-      } else {
-        let character = anime.characters.id(characterId);
-
-
-        if (character == null) {
-          res.status(404).json({ error: "Character not found." });
-        } else {
-          res.status(200).json(character);
-        }
-      }
-    };
-  };
 
 const findOne = function (req, res) {
-  if (req.params && req.params.id && req.params.characterId) {
-    let animeId = req.params.id;
-    let characterId = req.params.characterId;
+    const animeId = req.params.id;
+    const characterId = req.params.characterId;
 
-    if (!ObjectId.isValid(animeId) || !ObjectId.isValid(characterId)) {
-      res.status(400).json({ error: "Id is invalid." });
-      return;
+    const response = {
+        status: process.env.HTTP_STATUS_OKs,
+        data: null,
     }
 
-    character_findByIdCallback(animeId, characterId, character_onMongooseFindOneResponseCallback(req, res, characterId));
-  } else {
-    res.status(400).json({ error: "Anime Id and Character Id are required." });
-  }
-};
+    Anime_findById(animeId).then(anime => {
+        if (anime === null) {
+            response.status = process.env.HTTP_STATUS_NOT_FOUND;
+            response.data = {
+                error: process.env.ERROR_RESPONSE_ANIME_NOT_FOUND,
+            }
+        } else {
+            const character = anime.characters.id(characterId);
 
-const findAllWithPagination = function (req, res) {
-  let page = parseInt(process.env.QUERY_PAGE_DEFAULT);
-  let size = parseInt(process.env.QUERY_SIZE_DEFAULT);
-
-  if (req.query && req.query.page) {
-    page = parseInt(req.query.page);
-  }
-
-  if (req.query && req.query.size) {
-    size = parseInt(req.query.size);
-  }
-
-  if (isNaN(page) || isNaN(size) || page < 1 || size < 1 || size > 20) {
-    res.status(400).json({
-      error: "Page and size must be greater than one and size cannot be greater than 20.",
+            if (character === null) {
+                response.status = process.env.HTTP_STATUS_NOT_FOUND;
+                response.data = {
+                    error: process.env.ERROR_RESPONSE_CHARACTER_NOT_FOUND,
+                };
+            } else {
+                response.status = process.env.HTTP_STATUS_OK;
+                response.data = character;
+            }
+        }
+    }).catch((error) => {
+        response.status = process.env.HTTP_STATUS_INTERNAL_SERVER_ERROR;
+        response.data = process.env.ERROR_RESPONSE_SOMETHING_WENT_WRONG;
+    }).finally(() => {
+        sendResponse(res, response);
     });
-    return;
-  }
-
-  if (req.params && req.params.id) {
-    let animeId = req.params.id;
-
-    if (!ObjectId.isValid(animeId)) {
-      res.status(400).json({ error: "Anime Id is invalid." });
-      return;
-    }
-
-    character_findAllPaginatedCallback(animeId, page, size, character_onMongooseFindAllResponseCallback(req, res));
-  } else {
-    res.status(400).json({ error: "Anime Id is required." });
-  }
 };
+
 
 const addOne = function (req, res) {
-  if (!req.body) {
-    res.status(400).json({ error: "Body cannot be empty." });
-    return;
-  }
-
-  if (req.params && req.params.id) {
-    let animeId = req.params.id;
-
-    if (!ObjectId.isValid(animeId)) {
-      res.status(400).json({ error: "Anime Id is invalid." });
-      return;
-    }
+    const animeId = req.params.id;
 
     const character = {
-      name: req.body.name,
-      characteristics: req.body.characteristics,
-    };
+        name: req.body.name,
+        characteristics: req.body.characteristics,
+    }
 
-    character_createCallback(animeId, character, character_onMongooseResponseCallback(req, res));
-  } else {
-    res.status(400).json({ error: "Anime Id is required." });
-  }
+    const response = {
+        status: process.env.HTTP_STATUS_OKs,
+        data: null,
+    }
+
+    Anime_findById(animeId).then(anime => {
+        if (anime === null) {
+            response.status = process.env.HTTP_STATUS_NOT_FOUND;
+            response.data = {
+                error: process.env.ERROR_RESPONSE_ANIME_NOT_FOUND,
+            }
+
+            sendResponse(res, response);
+        } else {
+
+            anime.characters.push(character)
+
+            anime.save().then(
+                (newAnime) => {
+                    response.status = process.env.HTTP_STATUS_NO_CONTENT;
+                    response.data = anime;
+                }
+            ).catch(error => {
+                response.status = process.env.HTTP_STATUS_INTERNAL_SERVER_ERROR;
+                response.data = process.env.ERROR_RESPONSE_SOMETHING_WENT_WRONG;
+            }).finally(() => {
+                sendResponse(res, response);
+            });
+
+        }
+    }).catch((error) => {
+        console.log(error);
+
+        response.status = process.env.HTTP_STATUS_INTERNAL_SERVER_ERROR;
+        response.data = process.env.ERROR_RESPONSE_SOMETHING_WENT_WRONG;
+
+        sendResponse(res, response);
+    });
 };
 
 const updateOne = function (req, res) {
-  if (!req.body) {
-    res.status(400).json({ error: "Body cannot be empty." });
-    return;
-  }
+    const animeId = req.params.id;
+    const characterId = req.params.characterId;
 
-  if (req.params && req.params.id && req.params.characterId) {
-    let animeId = req.params.id;
-    let characterId = req.params.characterId;
-
-    if (!ObjectId.isValid(animeId) || !ObjectId.isValid(characterId)) {
-      res.status(400).json({ error: "Id is invalid." });
-      return;
+    const response = {
+        status: process.env.HTTP_STATUS_OKs,
+        data: null,
     }
 
-    const character = {
-      name: req.body.name,
-      characteristics: req.body.characteristics,
-    };
+    Anime_findById(animeId).then(anime => {
+        if (anime === null) {
+            response.status = process.env.HTTP_STATUS_NOT_FOUND;
+            response.data = {
+                error: process.env.ERROR_RESPONSE_ANIME_NOT_FOUND,
+            }
 
-    character_updateOneCallback(animeId, characterId, character, character_onMongooseResponseCallback(req, res));
-  } else {
-    res.status(400).json({ error: "Anime Id and Character Id are required." });
-  }
+            sendResponse(res, response);
+        } else {
+            const character = anime.characters.id(characterId);
+
+            if (character === null) {
+                response.status = process.env.HTTP_STATUS_NOT_FOUND;
+                response.data = {
+                    error: process.env.ERROR_RESPONSE_CHARACTER_NOT_FOUND,
+                };
+
+                sendResponse(res, response);
+            } else {
+
+                character.name = req.body.name;
+                character.characteristics = req.body.characteristics;
+
+                anime.save().then(
+                    (newAnime) => {
+                        response.status = process.env.HTTP_STATUS_OK;
+                        response.data = character;
+                    }
+                ).catch(error => {
+                    response.status = process.env.HTTP_STATUS_INTERNAL_SERVER_ERROR;
+                    response.data = process.env.ERROR_RESPONSE_SOMETHING_WENT_WRONG;
+                }).finally(() => {
+                    sendResponse(res, response);
+                });
+            }
+        }
+    }).catch((error) => {
+        console.log(error);
+
+        response.status = process.env.HTTP_STATUS_INTERNAL_SERVER_ERROR;
+        response.data = process.env.ERROR_RESPONSE_SOMETHING_WENT_WRONG;
+
+        sendResponse(res, response);
+    });
 };
 
 const partiallyUpdateOne = function (req, res) {
-  if (!req.body) {
-    res.status(400).json({ error: "Body cannot be empty." });
-    return;
-  }
+    const animeId = req.params.id;
+    const characterId = req.params.characterId;
 
-  if (req.params && req.params.id && req.params.characterId) {
-    let animeId = req.params.id;
-    let characterId = req.params.characterId;
-
-    if (!ObjectId.isValid(animeId) || !ObjectId.isValid(characterId)) {
-      res.status(400).json({ error: "Id is invalid." });
-      return;
+    const response = {
+        status: process.env.HTTP_STATUS_OKs,
+        data: null,
     }
 
-    const character = {
-      name: req.body.name,
-      characteristics: req.body.characteristics,
-    };
+    Anime_findById(animeId).then(anime => {
+        if (anime === null) {
+            response.status = process.env.HTTP_STATUS_NOT_FOUND;
+            response.data = {
+                error: process.env.ERROR_RESPONSE_ANIME_NOT_FOUND,
+            }
 
-    character_partiallyUpdateOneCallback(animeId, characterId, character, character_onMongooseResponseCallback(req, res));
-  } else {
-    res.status(400).json({ error: "Anime Id and Character Id are required." });
-  }
+            sendResponse(res, response);
+        } else {
+            const character = anime.characters.id(characterId);
+
+            if (character === null) {
+                response.status = process.env.HTTP_STATUS_NOT_FOUND;
+                response.data = {
+                    error: process.env.ERROR_RESPONSE_CHARACTER_NOT_FOUND,
+                };
+
+                sendResponse(res, response);
+            } else {
+
+                if (req && req.body.name) {
+                    character.name = req.body.name;
+                }
+                if (req && req.body.characteristics) {
+                    character.characteristics = req.body.characteristics;
+                }
+                
+                anime.save().then(
+                    (newAnime) => {
+                        response.status = process.env.HTTP_STATUS_OK;
+                        response.data = character;
+                    }
+                ).catch(error => {
+                    response.status = process.env.HTTP_STATUS_INTERNAL_SERVER_ERROR;
+                    response.data = process.env.ERROR_RESPONSE_SOMETHING_WENT_WRONG;
+                }).finally(() => {
+                    sendResponse(res, response);
+                });
+            }
+        }
+    }).catch((error) => {
+        console.log(error);
+
+        response.status = process.env.HTTP_STATUS_INTERNAL_SERVER_ERROR;
+        response.data = process.env.ERROR_RESPONSE_SOMETHING_WENT_WRONG;
+
+        sendResponse(res, response);
+    });
 };
 
 const deleteOne = function (req, res) {
-  if (req.params && req.params.id && req.params.characterId) {
-    let animeId = req.params.id;
-    let characterId = req.params.characterId;
+    const animeId = req.params.id;
+    const characterId = req.params.characterId;
 
-    if (!ObjectId.isValid(animeId) || !ObjectId.isValid(characterId)) {
-      res.status(400).json({ error: "Id is invalid." });
-      return;
+    const response = {
+        status: process.env.HTTP_STATUS_OKs,
+        data: null,
     }
 
-    character_deleteOneCallback(animeId, characterId, character_onMongooseResponseCallback(req, res));
-  } else {
-    res.status(400).json({ error: "Anime Id and Character Id are required." });
-  }
+    Anime_findById(animeId).then(anime => {
+        if (anime === null) {
+            response.status = process.env.HTTP_STATUS_NOT_FOUND;
+            response.data = {
+                error: process.env.ERROR_RESPONSE_ANIME_NOT_FOUND,
+            }
+
+            sendResponse(res, response);
+        } else {
+            const character = anime.characters.id(characterId);
+
+            if (character === null) {
+                response.status = process.env.HTTP_STATUS_NOT_FOUND;
+                response.data = {
+                    error: process.env.ERROR_RESPONSE_CHARACTER_NOT_FOUND,
+                };
+
+                sendResponse(res, response);
+            } else {
+                // TODO: FIX Later
+                anime.characters.pull({_id: characterId})
+
+                anime.save().then(
+                    (newAnime) => {
+                        response.status = process.env.HTTP_STATUS_OK;
+                        response.data = anime;
+                    }
+                ).catch(error => {
+                    response.status = process.env.HTTP_STATUS_INTERNAL_SERVER_ERROR;
+                    response.data = process.env.ERROR_RESPONSE_SOMETHING_WENT_WRONG;
+                }).finally(() => {
+                    sendResponse(res, response);
+                });
+            }
+        }
+    }).catch((error) => {
+        console.log(error);
+
+        response.status = process.env.HTTP_STATUS_INTERNAL_SERVER_ERROR;
+        response.data = process.env.ERROR_RESPONSE_SOMETHING_WENT_WRONG;
+
+        sendResponse(res, response);
+    });
 };
 
 module.exports = {
-  findOne,
-  findAllWithPagination,
-  addOne,
-  updateOne,
-  partiallyUpdateOne,
-  deleteOne,
+    findOne,
+    findAllWithPagination: findAll,
+    addOne,
+    updateOne,
+    partiallyUpdateOne,
+    deleteOne,
 };
