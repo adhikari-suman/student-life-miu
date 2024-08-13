21e4975e325de66e463424069e73e1dec88382db
Suman Adhikari <expertxkid@gmail.com>
2024-08-13 16:01:41 -0500

    refactor/PHP-MWA: refactor routes and server messages to use .env variables

--- 20240722_MWA/personal-hobby-projects/20240728-personal-hobby-project-backend/app.js
+++ 20240722_MWA/personal-hobby-projects/20240728-personal-hobby-project-backend/app.js
@@ -8,9 +8,11 @@ const app = express()
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
-app.use('/api/v1', routes);
+app.use(process.env.API_SUBSET_ROUTE_API_WITH_VERSION_V1, routes);
 
-const server = app.listen(process.env.PORT, function(){
-    console.log(`Listening on port: ${server.address().port}`)
-});
+const serverListenCallback = function () {
+    console.log(`${process.env.SERVER_MESSAGE_LISTENING_ON_PORT} ${server.address().port}`)
+};
+
+const server = app.listen(process.env.PORT, serverListenCallback);
 
--- 20240722_MWA/personal-hobby-projects/20240728-personal-hobby-project-backend/routes/index.js
+++ 20240722_MWA/personal-hobby-projects/20240728-personal-hobby-project-backend/routes/index.js
@@ -3,7 +3,7 @@ const animeController = require("../api/anime/anime.controller");
 const animeRouter = require("../api/anime/anime.router");
 const userRouter = require("../api/user/user.router");
 
-router.use('/anime', animeRouter);
-router.use('/users', userRouter);
+router.use(process.env.API_ANIME_ANIME_SUBSET_ROUTE_ANIME, animeRouter);
+router.use(process.env.API_USER_SUBSET_ROUTE_USERS, userRouter);
 
 module.exports = router;
