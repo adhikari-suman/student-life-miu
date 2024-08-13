f74b1a2a6473cd441bc85f1c765c837eb0179cd9
Suman Adhikari <expertxkid@gmail.com>
2024-08-12 21:50:44 -0500

    feat/PHP-MWA: refactor fullUpdateOne and partiallyUpdateOne in anime.controller.js to use chained named promises

--- 20240722_MWA/personal-hobby-projects/20240728-personal-hobby-project-backend/api/anime/anime.controller.js
+++ 20240722_MWA/personal-hobby-projects/20240728-personal-hobby-project-backend/api/anime/anime.controller.js
@@ -1,186 +1,7 @@
 const mongoose = require("mongoose");
 const ObjectId = mongoose.Types.ObjectId;
 const sendResponse = require("../../utils/send_response");
-const {parse} = require("dotenv");
-const callbackify = require("util").callbackify;
 const AnimeModel = mongoose.model(process.env.MONGODB_ANIME_MODEL_NAME);
-
-const HTTP_STATUS_OK = parseInt(process.env.HTTP_STATUS_OK)
-const HTTP_STATUS_BAD_REQUEST = parseInt(process.env.HTTP_STATUS_BAD_REQUEST)
-const HTTP_STATUS_NOT_FOUND = parseInt(process.env.HTTP_STATUS_NOT_FOUND)
-const HTTP_STATUS_INTERNAL_SERVER_ERROR = parseInt(process.env.HTTP_STATUS_INTERNAL_SERVER_ERROR)
-
-const anime_findAllPaginated = function (page, size) {
-    let offset = (page - 1) * size;
-
-    return AnimeModel.find().skip(offset).limit(size).exec();
-};
-
-const anime_findById = function (animeId) {
-    return AnimeModel.findById(animeId);
-};
-
-const anime_addOne = function (anime) {
-    return AnimeModel.create(anime);
-};
-
-const anime_saveOne = function (anime) {
-    return anime.save();
-};
-
-const anime_updateOne = function (animeId, anime) {
-    const filter = {_id: new ObjectId(animeId)};
-    const change = {$set: anime};
-
-    return AnimeModel.updateOne(filter, change);
-};
-
-const anime_deleteOne = function (animeId) {
-    const filter = {_id: new ObjectId(animeId)};
-
-    return AnimeModel.deleteOne(filter);
-};
-
-const anime_findAllPaginatedWithCallback = callbackify(anime_findAllPaginated);
-const anime_findByIdWithCallback = callbackify(anime_findById);
-const anime_createWithCallback = callbackify(anime_addOne);
-const anime_saveOneWithCallback = callbackify(anime_saveOne);
-const anime_updateOneWithCallback = callbackify(anime_updateOne);
-const anime_partiallyUpdateOneWithCallback = callbackify(anime_updateOne);
-const anime_deleteOneWithCallback = callbackify(anime_deleteOne);
-
-const anime_fullUpdateOne_onFindByIdWithCallback_HandleResponse = function (req, res) {
-    const anime_fullUpdateOne_onFindByIdWithCallback_HandleResponseHandler = function (error, anime) {
-        const response = {
-            status: HTTP_STATUS_OK,
-            data: null,
-        }
-
-        const anime_saveOneWithCallback_HandleResponse = function (err, anime) {
-            if (error) {
-                response.status = HTTP_STATUS_INTERNAL_SERVER_ERROR;
-                response.data = {
-                    error: process.env.ERROR_RESPONSE_INTERNAL_SERVER_ERROR,
-                };
-            } else {
-                response.status = HTTP_STATUS_OK;
-                response.data = anime;
-
-                console.log(response);
-            }
-
-            sendResponse(res, response);
-        };
-
-        if (error) {
-            response.status = HTTP_STATUS_INTERNAL_SERVER_ERROR;
-            response.data = {
-                error: process.env.ERROR_RESPONSE_INTERNAL_SERVER_ERROR,
-            };
-
-            sendResponse(res, response);
-        } else {
-            if (anime != null) {
-                anime.name = req.body.name;
-                anime.releaseDate = req.body.releaseDate;
-                anime.studio = req.body.studio;
-                anime.characters = req.body.characters;
-
-                anime_saveOneWithCallback(anime, anime_saveOneWithCallback_HandleResponse);
-            } else {
-                response.status = HTTP_STATUS_NOT_FOUND;
-                response.data = {
-                    error: process.env.ERROR_RESPONSE_ANIME_NOT_FOUND,
-                }
-
-                sendResponse(res, response);
-            }
-        }
-    }
-
-    return anime_fullUpdateOne_onFindByIdWithCallback_HandleResponseHandler;
-}
-
-const anime_partialUpdateOne_onFindByIdWithCallback_HandleResponse = function (req, res) {
-    const anime_partialUpdateOne_onFindByIdWithCallback_HandleResponseHandler = function (error, anime) {
-        const response = {
-            status: HTTP_STATUS_OK,
-            data: null,
-        }
-
-        const anime_saveOneWithCallback_HandleResponse = function (err, anime) {
-            if (error) {
-                response.status = HTTP_STATUS_INTERNAL_SERVER_ERROR;
-                response.data = {
-                    error: process.env.ERROR_RESPONSE_INTERNAL_SERVER_ERROR,
-                };
-            } else {
-                response.status = HTTP_STATUS_OK;
-                response.data = anime;
-            }
-
-            sendResponse(res, response);
-        };
-
-        if (error) {
-            response.status = HTTP_STATUS_INTERNAL_SERVER_ERROR;
-            response.data = {
-                error: process.env.ERROR_RESPONSE_INTERNAL_SERVER_ERROR,
-            };
-
-            sendResponse(res, response);
-        } else {
-            if (anime != null) {
-                if (req.body.name) {
-                    anime.name = req.body.name;
-                }
-
-                if (req.body.releaseDate) {
-                    anime.releaseDate = req.body.releaseDate;
-                }
-
-                if (req.body.studio) {
-                    anime.studio = req.body.studio;
-                }
-
-                if (req.body.characters) {
-                    anime.characters = req.body.characters;
-                }
-
-                anime_saveOneWithCallback(anime, anime_saveOneWithCallback_HandleResponse);
-            } else {
-                response.status = HTTP_STATUS_NOT_FOUND;
-                response.data = {
-                    error: process.env.ERROR_RESPONSE_ANIME_NOT_FOUND,
-                }
-
-                sendResponse(res, response);
-            }
-        }
-    }
-
-    return anime_partialUpdateOne_onFindByIdWithCallback_HandleResponseHandler;
-}
-
-const anime_onMongooseResponseCallback = function (res) {
-    const responseHandler = function (error, anime) {
-        if (error) {
-            res.status(HTTP_STATUS_INTERNAL_SERVER_ERROR).json({
-                error: process.env.ERROR_RESPONSE_SOMETHING_WENT_WRONG,
-            });
-        } else {
-            if (anime == null) {
-                res.status(HTTP_STATUS_NOT_FOUND).json({
-                    error: process.env.ERROR_RESPONSE_ANIME_NOT_FOUND,
-                });
-            } else {
-                res.status(HTTP_STATUS_OK).json(anime);
-            }
-        }
-    };
-
-    return responseHandler;
-};
 
 const _doesAnimeExist = anime => {
     return new Promise((resolve, reject) => {
@@ -302,12 +123,68 @@ const addOne = function (req, res) {
         .finally(() => sendResponse(res, response));
 };
 
+const _updateAnimeFull = (anime, update) => {
+    return new Promise((resolve, reject) => {
+        anime.name = update.name;
+        anime.releaseDate = update.releaseDate;
+        anime.studio = update.studio;
+        anime.characters = update.characters;
+
+        resolve(anime);
+    });
+};
+
+const _updateAnimePartial = (anime, update) => {
+    return new Promise((resolve, reject) => {
+        console.log(update);
+
+        if (update.name) {
+            anime.name = update.name;
+        }
+        if (update.releaseDate) {
+            anime.releaseDate = update.releaseDate;
+        }
+        if (update.studio) {
+            anime.studio = update.studio;
+        }
+        if (update.characters) {
+            anime.characters = update.characters;
+        }
+
+        resolve(anime);
+    });
+};
+
+const _updateOne = function (req, res, updateAnimeFunction) {
+    const animeId = req.params.id;
+
+    const update = {
+        name: req.body.name,
+        releaseDate: req.body.releaseDate,
+        studio: req.body.studio,
+        characters: req.body.characters
+    }
+
+    const response = {
+        status: parseInt(process.env.HTTP_STATUS_OK),
+        data: null,
+    }
+
+    AnimeModel.findById(animeId).exec()
+        .then(anime => _doesAnimeExist(anime))
+        .then(anime => updateAnimeFunction(anime, update))
+        .then((anime) => anime.save())
+        .then(anime => _setResponseToAnime(response, anime))
+        .catch(error => _setResponseToError(response, error))
+        .finally(() => sendResponse(res, response));
+}
+
 const fullUpdateOne = function (req, res) {
-    anime_findByIdWithCallback(req.params.id, anime_fullUpdateOne_onFindByIdWithCallback_HandleResponse(req, res));
+    _updateOne(req, res, _updateAnimeFull);
 };
 
 const partiallyUpdateOne = function (req, res) {
-    anime_findByIdWithCallback(req.params.id, anime_partialUpdateOne_onFindByIdWithCallback_HandleResponse(req, res));
+    _updateOne(req, res, _updateAnimePartial);
 };
 
 const _setResponseToDeleteAnimeSuccess = (response) => {
