e7e8a99a537a478f8b8087f5ff9c96efdc56d6a9
Suman Adhikari <expertxkid@gmail.com>
2024-08-13 16:25:31 -0500

    feat/PHP-MWA: use core modules header and footer in app module's app component

--- 20240722_MWA/personal-hobby-projects/20240728-personal-hobby-project-backend/public/php-angular-app/src/app/app.module.ts
+++ 20240722_MWA/personal-hobby-projects/20240728-personal-hobby-project-backend/public/php-angular-app/src/app/app.module.ts
@@ -1,18 +1,27 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {FooterComponent} from './core/footer/footer.component';
+import {HeaderComponent} from './core/header/header.component';
+import {NavigationComponent} from './core/navigation/navigation.component';
+import {CommonModule} from "@angular/common";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    HeaderComponent,
+    NavigationComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CommonModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
