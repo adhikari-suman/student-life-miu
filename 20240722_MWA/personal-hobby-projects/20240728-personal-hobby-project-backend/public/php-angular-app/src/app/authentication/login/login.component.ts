f358ae89a7a4299e552678d060db86514b1928dc
Suman Adhikari <expertxkid@gmail.com>
2024-08-13 20:07:52 -0500

    feat/PHP-MWA: add LoginComponent with login form

--- 20240722_MWA/personal-hobby-projects/20240728-personal-hobby-project-backend/public/php-angular-app/src/app/authentication/login/login.component.ts
+++ 20240722_MWA/personal-hobby-projects/20240728-personal-hobby-project-backend/public/php-angular-app/src/app/authentication/login/login.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormControl, FormGroup} from "@angular/forms";
+import {AuthenticationService} from "../authentication.service";
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrl: './login.component.css',
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  loginForm!: FormGroup;
 
+  constructor(private formBuilder: FormBuilder, private authenticationService: AuthenticationService) {
+  }
+
+  ngOnInit() {
+    this.loginForm = this.formBuilder.group({
+      username: new FormControl(null),
+      password: new FormControl(null),
+    });
+
+  }
+
+  loginPressed() {
+    this.authenticationService.login(
+      this.loginForm.value.username,
+      this.loginForm.value.password,
+    ).subscribe((detail) => {
+      console.log("login response", detail.token);
+      console.log("TODO: [loginPressed] - Save Token after successful login");
+    });
+  }
 }
