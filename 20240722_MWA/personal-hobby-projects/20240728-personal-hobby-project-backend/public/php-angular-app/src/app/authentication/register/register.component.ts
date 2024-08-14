77bc8e0cd62fef225f2be23663b058d0c92e8016
Suman Adhikari <expertxkid@gmail.com>
2024-08-14 10:09:54 -0500

    refactor/PHP-MWA: add a registration form

--- 20240722_MWA/personal-hobby-projects/20240728-personal-hobby-project-backend/public/php-angular-app/src/app/authentication/register/register.component.ts
+++ 20240722_MWA/personal-hobby-projects/20240728-personal-hobby-project-backend/public/php-angular-app/src/app/authentication/register/register.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {NgForm} from "@angular/forms";
+import {RegisterRequest} from "../models/register-request.model";
+import {AuthenticationService} from "../authentication.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-register',
@@ -7,4 +11,21 @@ import { Component } from '@angular/core';
 })
 export class RegisterComponent {
 
+  constructor(private _router: Router, private _authenticationService: AuthenticationService) {
+  }
+
+  registerPressed(registrationForm: NgForm) {
+    const registerRequest = new RegisterRequest(
+      registrationForm.form.value.username,
+      registrationForm.form.value.email,
+      registrationForm.form.value.password,
+      registrationForm.form.value.fullName,
+    );
+
+    this._authenticationService.register(registerRequest).subscribe(
+      () => {
+        this._router.navigate(['']);
+      }
+    );
+  }
 }
