22b435a700b0208cf68c7fbd45d222d73e22dbbc
Suman Adhikari <expertxkid@gmail.com>
2024-08-13 19:35:34 -0500

    feat/PHP-MWA: add environments for production and development

---
+++ 20240722_MWA/personal-hobby-projects/20240728-personal-hobby-project-backend/public/php-angular-app/src/environments/environment.production.ts
@@ -0,0 +1,30 @@
+const _getUserByIdEndpoint = function (userId: String): String {
+  return `/users/${userId}`;
+}
+
+const _getAnimePaginatedEndpoint = function (page: number = 1, size: number = 10): String {
+  return `/anime?page=${page}&size=${size}`;
+}
+
+const _animeByIdEndpoint = function (animeId: String): String {
+  return `/anime/${animeId}`;
+}
+
+const _getAnimeCharactersByAnimeIdEndpoint = function (animeId: String): String {
+  return `/anime/${animeId}/characters`;
+}
+
+const _animeCharacterByAnimeIdAndCharacterIdEndpoint = function (animeId: String, characterId: String): String {
+  return `/anime/${animeId}/characters/${characterId}`;
+}
+
+export const environment = {
+  baseUrl: 'http://localhost:3000/api/v1',
+  registerEndpoint: '/users',
+  loginEndpoint: '/users/authenticate',
+  getUserByIdEndpoint: _getUserByIdEndpoint,
+  getAnimePaginatedEndpoint: _getAnimePaginatedEndpoint,
+  animeByIdEndpoint: _animeByIdEndpoint,
+  getAnimeCharactersByAnimeIdEndpoint: _getAnimeCharactersByAnimeIdEndpoint,
+  animeCharacterByAnimeIdAndCharacterIdEndpoint: _animeCharacterByAnimeIdAndCharacterIdEndpoint,
+};
