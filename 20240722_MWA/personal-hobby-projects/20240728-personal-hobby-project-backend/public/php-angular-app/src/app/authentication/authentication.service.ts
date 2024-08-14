4d25d4368034aef0bbbfba971de49cb6e57a0541
Suman Adhikari <expertxkid@gmail.com>
2024-08-13 20:07:29 -0500

    feat/PHP-MWA: add AuthenticationService with login and register method

--- 20240722_MWA/personal-hobby-projects/20240728-personal-hobby-project-backend/public/php-angular-app/src/app/authentication/authentication.service.ts
+++ 20240722_MWA/personal-hobby-projects/20240728-personal-hobby-project-backend/public/php-angular-app/src/app/authentication/authentication.service.ts
@@ -1,10 +1,40 @@
 import {Injectable} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {environment} from "../../environments/environment";
+import {Observable} from "rxjs";
+import {LoginResponse} from "./models/login-response.model";
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthenticationService {
 
-  constructor() {
+  constructor(private _http: HttpClient) {
   }
+
+  login(username: string, password: string): Observable<LoginResponse> {
+    const loginUrl = `${environment.baseUrl}${environment.loginEndpoint}`;
+
+    const loginDetail = {
+      username: username,
+      password: password
+    }
+
+    return this._http.post<LoginResponse>(loginUrl, loginDetail);
+  }
+
+  register(username: string, password: string, email: string, fullName: string): Observable<void> {
+    const registerUrl = `${environment.baseUrl}${environment.registerEndpoint}`;
+
+    const registerDetail = {
+      username: username,
+      password: password,
+      email: email,
+      fullName: fullName,
+    }
+
+    return this._http.post<void>(registerUrl, registerDetail);
+  }
+
+
 }
