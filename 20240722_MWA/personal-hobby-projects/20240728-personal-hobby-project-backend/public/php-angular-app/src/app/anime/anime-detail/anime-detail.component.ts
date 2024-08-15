5882638abc7edab76bb6383328dbca9e83253095
Suman Adhikari <expertxkid@gmail.com>
2024-08-15 09:59:03 -0500

    feat/PHP-MWA: add edit and delete button on anime-detail.component.ts

--- 20240722_MWA/personal-hobby-projects/20240728-personal-hobby-project-backend/public/php-angular-app/src/app/anime/anime-detail/anime-detail.component.ts
+++ 20240722_MWA/personal-hobby-projects/20240728-personal-hobby-project-backend/public/php-angular-app/src/app/anime/anime-detail/anime-detail.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Anime} from "../anime.model";
+import {AnimeService} from "../anime.service";
+import {ActivatedRoute, Router} from "@angular/router";
+import {AuthenticationService} from "../../authentication/authentication.service";
 
 @Component({
   selector: 'app-anime-detail',
   templateUrl: './anime-detail.component.html',
   styleUrl: './anime-detail.component.css'
 })
-export class AnimeDetailComponent {
+export class AnimeDetailComponent implements OnInit {
 
+  animeDetail: Anime | null = null;
+
+  constructor(
+    private _animeService: AnimeService,
+    private _activatedRoute: ActivatedRoute,
+    private _authenticationService: AuthenticationService,
+    private _router: Router,
+  ) {
+  }
+
+  get isLoggedIn(): boolean {
+    return this._authenticationService.isUserLoggedIn;
+  }
+
+
+  ngOnInit() {
+    const animeId = this._activatedRoute.snapshot.params['id'];
+
+    this._animeService.getOne(animeId).subscribe(anime => {
+      this.animeDetail = anime;
+    })
+  }
+
+  deletePressed(animeId: string) {
+    this._animeService.deleteOne(animeId).subscribe(anime => {
+      this._router.navigate(["anime"]);
+    });
+  }
 }
