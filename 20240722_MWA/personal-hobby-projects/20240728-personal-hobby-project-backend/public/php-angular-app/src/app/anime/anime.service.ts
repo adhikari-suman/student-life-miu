c12ebd749d9ec761e2b5287672adb3ca31ae6b71
Suman Adhikari <expertxkid@gmail.com>
2024-08-14 20:11:45 -0500

    refactor/PHP-MWA: add crud operations for anime in anime.service.ts

--- 20240722_MWA/personal-hobby-projects/20240728-personal-hobby-project-backend/public/php-angular-app/src/app/anime/anime.service.ts
+++ 20240722_MWA/personal-hobby-projects/20240728-personal-hobby-project-backend/public/php-angular-app/src/app/anime/anime.service.ts
@@ -1,9 +1,45 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {environment} from "../../environments/environment";
+import {Anime} from "./anime.model";
+import {Observable} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
 export class AnimeService {
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+  }
+
+  getAll(): Observable<Anime[]> {
+    const url = `${environment.baseUrl}${environment.getAnimePaginatedEndpoint()}`;
+
+    return this.http.get<Anime[]>(url);
+
+  }
+
+  getOne(id: string): Observable<Anime> {
+    const url = `${environment.baseUrl}${environment.animeByIdEndpoint(id)}`;
+
+    return this.http.get<Anime>(url);
+  }
+
+  addOne(anime: Anime): Observable<void> {
+    const url = `${environment.baseUrl}${environment.animeEndpoint}`;
+
+    return this.http.post<void>(url, anime);
+  }
+
+  updateOne(id: string, anime: any): Observable<void> {
+    const url = `${environment.baseUrl}${environment.animeByIdEndpoint(id)}`;
+
+    return this.http.put<void>(url, anime);
+  }
+
+  deleteOne(id: string): Observable<void> {
+    const url = `${environment.baseUrl}${environment.animeByIdEndpoint(id)}`;
+
+    return this.http.delete<void>(url);
+  }
 }
