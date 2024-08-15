98370718bef3c7a60fd00b03f60f16d24e602b76
Suman Adhikari <expertxkid@gmail.com>
2024-08-15 09:28:55 -0500

    feat/PHP-MWA: add NotFoundComponent in core module

--- 20240722_MWA/personal-hobby-projects/20240728-personal-hobby-project-backend/public/php-angular-app/src/app/core/core.module.ts
+++ 20240722_MWA/personal-hobby-projects/20240728-personal-hobby-project-backend/public/php-angular-app/src/app/core/core.module.ts
@@ -6,27 +6,29 @@ import {NavigationComponent} from "./navigation/navigation.component";
 import {ErrorComponent} from './error/error.component';
 import {RouterLink} from "@angular/router";
 import {AuthenticationModule} from "../authentication/authentication.module";
+import {NotFoundComponent} from './not-found/not-found.component';
 
 
 @NgModule({
-    declarations: [
-        HeaderComponent,
-        FooterComponent,
-        NavigationComponent,
-        ErrorComponent,
-    ],
-    imports: [
-        CommonModule,
-        RouterLink,
-        AuthenticationModule,
-    ],
-    exports: [
-        HeaderComponent,
-        FooterComponent,
-        NavigationComponent,
-        FooterComponent,
-        ErrorComponent,
-    ]
+  declarations: [
+    HeaderComponent,
+    FooterComponent,
+    NavigationComponent,
+    ErrorComponent,
+    NotFoundComponent,
+  ],
+  imports: [
+    CommonModule,
+    RouterLink,
+    AuthenticationModule,
+  ],
+  exports: [
+    HeaderComponent,
+    FooterComponent,
+    NavigationComponent,
+    FooterComponent,
+    ErrorComponent,
+  ]
 })
 export class CoreModule {
 }
