bb50514cfbd0497215521ee84557b45579d269b7
Suman Adhikari <expertxkid@gmail.com>
2024-08-14 20:15:53 -0500

    refactor/PHP-MWA: fetch anime from api and show in anime list

--- 20240722_MWA/personal-hobby-projects/20240728-personal-hobby-project-backend/public/php-angular-app/src/app/anime/anime-list/anime-list.component.ts
+++ 20240722_MWA/personal-hobby-projects/20240728-personal-hobby-project-backend/public/php-angular-app/src/app/anime/anime-list/anime-list.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {AnimeService} from "../anime.service";
+import {Anime} from "../anime.model";
 
 @Component({
   selector: 'app-anime-list',
   templateUrl: './anime-list.component.html',
   styleUrl: './anime-list.component.css'
 })
-export class AnimeListComponent {
+export class AnimeListComponent implements OnInit {
+  #anime: Anime[] = [];
 
+  get anime(): Anime[] {
+    return this.#anime;
+  }
+
+  set anime(value: Anime[]) {
+    this.#anime = value;
+  }
+
+  constructor(private _animeService: AnimeService) {
+  }
+
+  ngOnInit() {
+    this._animeService.getAll().subscribe(animeList => {
+      this.#anime = animeList;
+    })
+  }
 }
