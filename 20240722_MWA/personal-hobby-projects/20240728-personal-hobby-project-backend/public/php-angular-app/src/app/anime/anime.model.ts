8395e06c9f8091e431849229c9002703199c8d7c
Suman Adhikari <expertxkid@gmail.com>
2024-08-15 09:58:42 -0500

    feat/PHP-MWA: add studio in anime.model.ts

--- 20240722_MWA/personal-hobby-projects/20240728-personal-hobby-project-backend/public/php-angular-app/src/app/anime/anime.model.ts
+++ 20240722_MWA/personal-hobby-projects/20240728-personal-hobby-project-backend/public/php-angular-app/src/app/anime/anime.model.ts
@@ -4,16 +4,19 @@ export class Anime {
   public _id?: string;  // Optional property
   public name: string;
   public characters: Character[];
+  public studio: string;
   public releaseDate: Date;
 
   constructor(
     releaseDate: Date,
     name: string,
     characters: Character[],
+    studio: string,
     _id?: string,
   ) {
     this._id = _id;
     this.name = name;
+    this.studio = studio;
     this.characters = characters;
     this.releaseDate = releaseDate;
   }
