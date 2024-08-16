777bd5830e7dd08503066b1e9c6761e2633c86e3
Suman Adhikari <expertxkid@gmail.com>
2024-08-15 22:38:33 -0500

    feat/PHP-MWA: use cors headers from .env files

--- 20240722_MWA/personal-hobby-projects/20240728-personal-hobby-project-backend/app.js
+++ 20240722_MWA/personal-hobby-projects/20240728-personal-hobby-project-backend/app.js
@@ -5,6 +5,15 @@ const routes = require('./routes');
 
 const app = express()
 
+const _corsMiddleware = function (req, res, next) {
+    res.header(process.env.CORS_HEADER_ACCESS_CONTROL_ALLOW_ORIGIN_KEY, process.env.CORS_HEADER_ACCESS_CONTROL_ALLOW_ORIGIN_VALUE);
+    res.header(process.env.CORS_HEADER_ACCESS_CONTROL_ALLOW_HEADERS_KEY, process.env.CORS_HEADER_ACCESS_CONTROL_ALLOW_HEADERS_VALUE);
+    res.header(process.env.CORS_HEADER_ACCESS_CONTROL_ALLOW_METHODS_KEY, process.env.CORS_HEADER_ACCESS_CONTROL_ALLOW_METHODS_VALUE);
+
+    next();
+};
+
+app.use(_corsMiddleware);
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
