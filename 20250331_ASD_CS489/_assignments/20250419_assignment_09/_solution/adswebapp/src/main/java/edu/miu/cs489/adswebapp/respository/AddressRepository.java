e9d7a3b6e48cc5452945a3a728202b14d5551149
Suman Adhikari <expertxkid@gmail.com>
2025-04-19 12:07:37 -0500

    refactor/CS489::Lab9: add size() method

--- 20250331_ASD_CS489/_assignments/20250419_assignment_09/_solution/adswebapp/src/main/java/edu/miu/cs489/adswebapp/respository/AddressRepository.java
+++ 20250331_ASD_CS489/_assignments/20250419_assignment_09/_solution/adswebapp/src/main/java/edu/miu/cs489/adswebapp/respository/AddressRepository.java
@@ -2,6 +2,11 @@ package edu.miu.cs489.adswebapp.respository;
 
 import edu.miu.cs489.adswebapp.model.Address;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface AddressRepository extends JpaRepository<Address, Integer> {
+
+    @Query("SELECT COUNT(a) FROM Address a")
+    long size();
+
 }
