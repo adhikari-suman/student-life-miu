65e82138fbd30c56d5b5f972a5738fe3d9d1e535
Suman Adhikari <expertxkid@gmail.com>
2025-04-19 16:32:59 -0500

    feat/CS489::Lab9: add initial filter chain in SecurityConfiguration

---
+++ 20250331_ASD_CS489/_assignments/20250419_assignment_09/_solution/adswebapp/src/main/java/edu/miu/cs489/adswebapp/security/configuration/SecurityConfiguration.java
@@ -0,0 +1,40 @@
+package edu.miu.cs489.adswebapp.security.configuration;
+
+import edu.miu.cs489.adswebapp.respository.UserRepository;
+import edu.miu.cs489.adswebapp.security.filter.JwtFilter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@RequiredArgsConstructor
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class SecurityConfiguration {
+    private final JwtFilter              jwtFilter;
+    private final AuthenticationProvider authenticationProvider;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(CsrfConfigurer::disable)
+            .authorizeHttpRequests(authorizeRequests -> authorizeRequests.requestMatchers("/api/v1/auth/*")
+                                                                         .permitAll()
+                                                                         .anyRequest()
+                                                                         .authenticated())
+            .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
+            .authenticationProvider(authenticationProvider)
+            .sessionManagement(
+                    httpSecuritySessionManagementConfigurer -> httpSecuritySessionManagementConfigurer.sessionCreationPolicy(
+                            SessionCreationPolicy.STATELESS));
+
+        return http.build();
+    }
+}
