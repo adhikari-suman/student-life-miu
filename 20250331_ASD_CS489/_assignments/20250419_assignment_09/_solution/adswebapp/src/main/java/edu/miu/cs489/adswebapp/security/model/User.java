ae35f80054a2a361300fbc52418f79dde67a9dfa
Suman Adhikari <expertxkid@gmail.com>
2025-04-19 12:36:56 -0500

    feat/CS489::Lab9: extend the UserDetails for User model

--- 20250331_ASD_CS489/_assignments/20250419_assignment_09/_solution/adswebapp/src/main/java/edu/miu/cs489/adswebapp/security/model/User.java
+++ 20250331_ASD_CS489/_assignments/20250419_assignment_09/_solution/adswebapp/src/main/java/edu/miu/cs489/adswebapp/security/model/User.java
@@ -4,6 +4,11 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
 
 @Entity
 @Table(name = "users")
@@ -11,20 +16,38 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class User {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
-    @Column(name = "first_name")
-    private String firstName;
-    @Column(name = "last_name")
-    private String lastName;
-    @Column(name = "username", unique = true, nullable = false, length = 50)
-    private String username;
-    @Column(name = "password_hash")
-    private String password;
-    @Column(name = "phone_number", nullable = false, length = 20)
-    private String phoneNumber;
-    @Column(name="email", unique = true, nullable = false, length = 100)
-    private String email;
+public abstract class User implements UserDetails {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY) private                  Integer id;
+    @Column(name = "first_name") private                                             String  firstName;
+    @Column(name = "last_name") private                                              String  lastName;
+    @Column(name = "username", unique = true, nullable = false, length = 50) private String  username;
+    @Column(name = "password_hash") private                                          String  password;
+    @Column(name = "phone_number", nullable = false, length = 20) private            String  phoneNumber;
+    @Column(name = "email", unique = true, nullable = false, length = 100) private   String  email;
+    @Enumerated(EnumType.STRING) @Column(name = "role", nullable = false) private    Role    role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return role.getAuthorities();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return UserDetails.super.isEnabled();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
+    }
 }
