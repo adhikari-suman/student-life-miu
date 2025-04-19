4e370f26f015121176249afefea66bf478e93526
Suman Adhikari <expertxkid@gmail.com>
2025-04-19 11:54:04 -0500

    feat/CS489::Lab9: add security and jjwt package

--- 20250331_ASD_CS489/_assignments/20250419_assignment_09/_solution/adswebapp/build.gradle.kts
+++ 20250331_ASD_CS489/_assignments/20250419_assignment_09/_solution/adswebapp/build.gradle.kts
@@ -29,6 +29,9 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.mapstruct:mapstruct:1.6.3")
+    implementation("io.jsonwebtoken:jjwt-api:0.12.6")
+    implementation("org.springframework.boot:spring-boot-starter-security")
+    testImplementation("org.springframework.security:spring-security-test")
 
     annotationProcessor("org.projectlombok:lombok")
     annotationProcessor("org.mapstruct:mapstruct-processor:1.6.3")
@@ -38,8 +41,8 @@ dependencies {
     developmentOnly("org.springframework.boot:spring-boot-devtools")
 
     runtimeOnly("org.postgresql:postgresql")
-
-
+    runtimeOnly("io.jsonwebtoken:jjwt-impl:0.12.6")
+    runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.12.6")
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
