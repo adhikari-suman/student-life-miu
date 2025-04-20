73c45572f7c538d51f63bbc93421d56483999d10
Suman Adhikari <expertxkid@gmail.com>
2025-04-19 19:57:06 -0500

    refactor/CS489::Lab9: add existsUserByUsername(:username) in UserRepository

--- 20250331_ASD_CS489/_assignments/20250419_assignment_09/_solution/adswebapp/src/main/java/edu/miu/cs489/adswebapp/respository/UserRepository.java
+++ 20250331_ASD_CS489/_assignments/20250419_assignment_09/_solution/adswebapp/src/main/java/edu/miu/cs489/adswebapp/respository/UserRepository.java
@@ -2,9 +2,13 @@ package edu.miu.cs489.adswebapp.respository;
 
 import edu.miu.cs489.adswebapp.security.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
     Optional<User> findByUsername(String username);
+
+    boolean existsUserByUsername(String username);
 }
